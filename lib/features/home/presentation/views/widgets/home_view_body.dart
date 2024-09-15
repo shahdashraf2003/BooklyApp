4cf80d896e4704fb7960d315d77dac8cd6261970
shahdashraf2003 <shahdashraf396@gmail.com>
2024-09-15 19:32:07 +0300

    create style file & add title to home

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/featured_list_view.dart';
 import 'package:flutter/material.dart';
@@ -7,12 +8,20 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        CustomAppBar(),
-        FeaturedListView(),
-      ],
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeaturedListView(),
+          SizedBox(height: 40),
+          Text(
+            'Best Seller',
+            style: Styles.mediumTitle,
+          ),
+        ],
+      ),
     );
   }
 }
-
