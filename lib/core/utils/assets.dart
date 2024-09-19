171bb27600d5feb00ce8b6d88071327880b1e758
shahdashraf2003 <shahdashraf396@gmail.com>
2024-09-19 21:34:01 +0300

    add play image to custom list view item

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,4 +1,6 @@
 class AssetsData {
   static const logo = 'assets/images/Logo.png';
   static const test = 'assets/images/test.png';
+  static const oval = 'assets/images/Oval.png';
+  static const playIcon = 'assets/images/playicon.png';
 }
--- lib/features/home/presentation/views/widgets/custom_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -6,18 +6,37 @@ class FeaturedCustomListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  AspectRatio(
-        aspectRatio: 2.6 / 4,
-        child: Container(
-          decoration: const BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(16)),
-            image: DecorationImage(
-              image: AssetImage(AssetsData.test),
-              fit: BoxFit.fill,
+    return Stack(
+      children: [
+        AspectRatio(
+          aspectRatio: 2.6 / 4,
+          child: Container(
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(16)),
+              image: DecorationImage(
+                image: AssetImage(AssetsData.test),
+                fit: BoxFit.fill,
+              ),
             ),
           ),
         ),
-      
+        Positioned(
+          right: -27,
+          bottom: -22,
+          child: Image(
+              image: Image.asset(AssetsData.oval).image,
+              fit: BoxFit.fill,
+              color: const Color.fromARGB(255, 255, 255, 255)),
+        ),
+        Positioned(
+          bottom: 30,
+          right: 18,
+          child: Image(
+            image: Image.asset(AssetsData.playIcon).image,
+            color: Colors.white,
+          ),
+        ),
+      ],
     );
   }
 }
