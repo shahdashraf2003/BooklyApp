80336faa265094e0a39098cbd36ffc7c35b2b3fe
shahdashraf2003 <shahdashraf396@gmail.com>
2024-09-25 21:29:15 +0300

    craete search view

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/features/home/presentation/views/book_details_view.dart';
 import 'package:bookly_app/features/home/presentation/views/home_view.dart';
+import 'package:bookly_app/features/search/presentation/views/search_view.dart';
 import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
 import 'package:bookly_app/helper/constants.dart';
 import 'package:flutter/material.dart';
@@ -8,6 +9,8 @@ import 'package:go_router/go_router.dart';
 abstract class AppRouter {
   static const kHomeView = '/homeView';
   static const kBookDetailsView = '/bookDetailsView';
+    static const kSearchView = '/searchView';
+
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -32,6 +35,10 @@ abstract class AppRouter {
        GoRoute(
         path: kBookDetailsView,
         builder: (context, state) => const BookDetailsView(),
+      ),
+       GoRoute(
+        path: kSearchView,
+        builder: (context, state) => const SearchView(),
       ),
     ],
   );
