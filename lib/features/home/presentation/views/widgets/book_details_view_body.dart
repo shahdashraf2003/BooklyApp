dae449e2ad6a2ddf2267aee6f599a92f209015f5
shahdashraf2003 <shahdashraf396@gmail.com>
2024-09-25 20:24:05 +0300

    add CustomScrollView

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -12,64 +12,73 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 33,
-      ),
-      child: Column(
-        children: [
-          const CustomBookDetailsAppBar(),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * 0.2),
-            child: const CustomLBookImage(),
+    return CustomScrollView(
+      slivers: [
+      SliverFillRemaining(
+        hasScrollBody: false,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 33,
           ),
-          const SizedBox(height: 43),
-          Text(
-            'The Jungle Book',
-            style: Styles.textStyle30.copyWith(
-              fontWeight: FontWeight.normal,
-              fontFamily: 'kGTSectraFine',
-            ),
-          ),
-          const SizedBox(height: 8),
-          Opacity(
-            opacity: 0.7,
-            child: Text(
-              'Rudyard Kipling',
-              style: Styles.textStyle18.copyWith(
-                fontWeight: FontWeight.normal,
-                fontStyle: FontStyle.italic,
+          child: Column(
+            children: [
+              const CustomBookDetailsAppBar(),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: width * 0.2),
+                child: const CustomLBookImage(),
               ),
-            ),
-          ),
-          const SizedBox(height: 17),
-          const BookRating(
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-          const SizedBox(
-            height: 37,
-          ),
-          const BookActions(),
-          const SizedBox(
-            height: 50,
-          ),
-          Align(
-            alignment: Alignment.bottomLeft,
-            child: Text(
-              'You can also like',
-              style: Styles.textStyle14.copyWith(
-                fontWeight: FontWeight.w600,
+              const SizedBox(height: 43),
+              Text(
+                'The Jungle Book',
+                style: Styles.textStyle30.copyWith(
+                  fontWeight: FontWeight.normal,
+                  fontFamily: 'kGTSectraFine',
+                ),
               ),
-            ),
+              const SizedBox(height: 8),
+              Opacity(
+                opacity: 0.7,
+                child: Text(
+                  'Rudyard Kipling',
+                  style: Styles.textStyle18.copyWith(
+                    fontWeight: FontWeight.normal,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 17),
+              const BookRating(
+                mainAxisAlignment: MainAxisAlignment.center,
+              ),
+              const SizedBox(
+                height: 37,
+              ),
+              const BookActions(),
+              const Expanded(
+                child: SizedBox(
+                  height: 50,
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'You can also like',
+                  style: Styles.textStyle14.copyWith(
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              const SimilarBooksListView(),
+              const SizedBox(
+                height: 40,
+              ),
+            ],
           ),
-          const SizedBox(
-            height: 16,
-          ),
-          const SimilarBooksListView(),
-         
-        ],
+        ),
       ),
-    );
+    ]);
   }
 }
-
