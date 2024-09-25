89bb00cdd97338200976c234573959210e940312
shahdashraf2003 <shahdashraf396@gmail.com>
2024-09-25 22:06:24 +0300

    search view done

--- lib/features/home/presentation/views/widgets/best_seller_lsliver_list.dart
+++ lib/features/home/presentation/views/widgets/best_seller_lsliver_list.dart
@@ -1,4 +1,4 @@
-import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/book_list_view_item.dart';
 import 'package:flutter/material.dart';
 
 class BestSellerList extends StatelessWidget {
@@ -11,7 +11,7 @@ class BestSellerList extends StatelessWidget {
         (BuildContext context, int index) {
           return const Padding(
             padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-            child: BestSellerListViewItem(),
+            child: BookListViewItem(),
           ); // Replace with your item widget
         },
         childCount: 10,
--- lib/features/search/presentation/views/widget/search_view_body.dart
+++ lib/features/search/presentation/views/widget/search_view_body.dart
@@ -1,19 +1,54 @@
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/book_list_view_item.dart';
+import 'package:bookly_app/features/search/presentation/views/widget/custom_search_text_field.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 28, vertical: 2),
-      child: Column(
-        children: [
-          CustomSearchTextFiled(),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 28, vertical: 2),
+      child: CustomScrollView(
+        slivers: [
+          SliverFillRemaining(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const CustomSearchTextFiled(),
+                const SizedBox(height: 16),
+                Text(
+                  'Search Result',
+                  style: Styles.textStyle16.copyWith(
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(height: 16),
+                const Expanded(child: SerachResultListView()),
+              ],
+            ),
+          ),
         ],
       ),
     );
   }
 }
 
+class SerachResultListView extends StatelessWidget {
+  const SerachResultListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 10,
+      padding: EdgeInsets.zero,
+      itemBuilder: (context, index) {
+        return const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+          child: BookListViewItem(),
+        );
+      },
+    );
+  }
+}
