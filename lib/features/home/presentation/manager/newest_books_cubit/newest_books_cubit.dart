b827fbcd651ebb7339b335fcf7056b3e7f750e6b
shahdashraf2003 <shahdashraf396@gmail.com>
2024-10-02 20:42:11 +0300

    newest books cubit done

--- lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
+++ lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -1,9 +1,20 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit() : super(NewestBooksInitial());
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchNewsetBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchNewsetBooks();
+    result.fold((failure) {
+      emit(NewestBooksFailure(failure.errMessage));
+    }, (books) {
+      emit(NewestBooksSuccess(books));
+    });
+  }
 }
--- lib/features/home/presentation/manager/newest_books_cubit/newest_books_state.dart
+++ lib/features/home/presentation/manager/newest_books_cubit/newest_books_state.dart
@@ -11,12 +11,12 @@ final class NewestBooksInitial extends NewestBooksState {}
 final class NewestBooksLoading extends NewestBooksState {}
 final class NewestBooksFailure extends NewestBooksState {
   final String errMessage;
-  const NewestBooksFailure({required this.errMessage});
+  const NewestBooksFailure(this.errMessage);
   
 }
 final class NewestBooksSuccess extends NewestBooksState {
   final List<BookModel> books;
-  const NewestBooksSuccess({required this.books});
+  const NewestBooksSuccess( this.books);
   
   
 }
