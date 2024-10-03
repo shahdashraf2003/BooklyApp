80ade5ad8e108192843903875b045a4bb6ff6f34
shahdashraf2003 <shahdashraf396@gmail.com>
2024-10-03 18:17:26 +0300

    add get it to our app

--- lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
+++ lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -1,7 +1,7 @@
-import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'newest_books_state.dart';
 
